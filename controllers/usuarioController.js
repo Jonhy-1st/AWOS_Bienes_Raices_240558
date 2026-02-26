c6e059406c305f6f9f5de226d7e4c4f262196811
Jonyh-1st <240558@utxicotepec.edu.mx>
2026-02-25 20:49:38 -0600

    Bienes Raíces
    update: Se actualizó el codigo de controllador, y de las views de registro para agregar un formulario de login. add: Se agregó el formulario de creación de contraseña tambien la navegación entre paginas

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,19 +1,17 @@
 const formularioLogin = (req, res) => {
-    res.render('auth/login', {
-        autentificado: false,
-        nombre: '',
-        titulo: 'Iniciar Sesión'
-    });
     res.render('auth/login', {pagina: "Inicia Sesión"});
+    res.render('auth/login', {pagina: "Ingresa los datos de la cuenta"});
 }
 
 const formularioRegistro = (req, res) => {
-    res.render('auth/registro', {
-        
-    });
     res.render('auth/registro',  {pagina: "Registrate con nosotros"});
 }
 
+const formualrioRecuperar = (req, res) => {
+    res.render('auth/recuperar', {pagina: "Te enviaremos un email con la liga de restauración de contraseña"});
+}
+
 export {
-    formularioLogin, formularioRegistro
+    formularioLogin, formularioRegistro,
+    formularioLogin, formularioRegistro, formualrioRecuperar
 }
